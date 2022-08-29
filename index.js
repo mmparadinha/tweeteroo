7b04da9b054b948ac7fda4a0f132f91b55dbd4a2
mmparadinha <matheusmparadinha@gmail.com>
2022-08-29 16:43:56 -0300

    Add validation for user input Add status code error/check messages Add query selector to filter by username Add tweet's username received via headers

--- index.js
+++ index.js
@@ -6,33 +6,53 @@ app.use(express.json());
 app.use(cors());
 
 const users = [];
-const tweets = [];
+const allTweets = [];
+
+function isURL(url) {
+    return (url.match(/https:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)?/gi));
+}
 
 app.post('/sign-up', (req, res) => {
-    users.push(req.body);
-    res.send('OK');
+    if (req.body.username !== '' && isURL(req.body.avatar)) {
+        users.push(req.body);
+        res.status(201).send('OK');
+    } else {
+        res.status(400).send('Todos os campos são obrigatórios');
+    };
 });
 
 app.post('/tweets', function (req, res) {
-    tweets.push({
-        ...req.body,
-        id: tweets.length+1
-    });
-    res.send('OK');
+    if (req.headers.user !== '' && req.body.tweet !== '') {
+        allTweets.push({
+            user: req.headers.user,
+            ...req.body,
+            id: allTweets.length+1
+        });
+        res.status(201).send('OK');
+    } else {
+        res.status(400).send('Todos os campos são obrigatórios');
+    };
+    console.log(allTweets)
 });
 
 app.get('/tweets', function (req, res) {
-    let lastTweets = [];
+    const username = req.query.username;
     const tweetsShown = 10;
-    const renderLimit = (tweetsShown > tweets.length ? tweets.length : tweetsShown);
+    let renderLimit = 0;
+    let lastTweets = [];
+    let tweets = [];
+
+    (username ? tweets = allTweets.filter(tweet => tweet.user === username) : tweets = allTweets);
+    renderLimit = (tweetsShown > tweets.length ? tweets.length : tweetsShown);
 
     for (let i = tweets.length-1; lastTweets.length < renderLimit; i--) {
-        const userAvatar = users.find(user => user.username === tweets[i].username).avatar;
+        const userAvatar = users.find(user => user.username === tweets[i].user).avatar;
         lastTweets.push({
             ...tweets[i],
             avatar: userAvatar
         });
     };
+    
     res.send(lastTweets);
 });
 
